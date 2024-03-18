6e18e7a73d80cef8e11bb38691a0690a8de76c7b
anastasia-sl <anastasiaslynko2206@gmail.com>
2024-03-18 18:25:28 +0300

    change functionality according to the task

--- lab1/index.js
+++ lab1/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 // Функція для перетворення тексту Markdown в HTML
-function markdownToHTML(markdownText) {
+function markdownToHTML(markdownText, format = 'ansi') {
     function escapeHTML(text) {
         return text.replace(/&/g, '&amp;')
             .replace(/</g, '&lt;')
@@ -12,19 +12,19 @@ function markdownToHTML(markdownText) {
 
     // Обгортка для жирного тексту
     const boldRegex = /\*\*(.*?)\*\*/g;
-    markdownText = markdownText.replace(boldRegex, '<strong>$1</strong>');
+    markdownText = markdownText.replace(boldRegex, format === 'ansi' ? '\x1b[1m$1\x1b[22m' : '<strong>$1</strong>');
 
     // Обгортка для курсивного тексту
     const italicRegex = /_(.*?)_/g;
-    markdownText = markdownText.replace(italicRegex, '<em>$1</em>');
+    markdownText = markdownText.replace(italicRegex, format === 'ansi' ? '\x1b[3m$1\x1b[23m' : '<em>$1</em>');
 
     // Обгортка для моноширинного тексту
     const monospacedRegex = /`([^`]+)`/g;
-    markdownText = markdownText.replace(monospacedRegex, '<code>$1</code>');
+    markdownText = markdownText.replace(monospacedRegex, format === 'ansi' ? '\x1b[7m$1\x1b[27m' : '<code>$1</code>');
 
     // Обгортка для блоків передформатованого тексту
     const preformattedRegex = /```([^`]+)```/g;
-    markdownText = markdownText.replace(preformattedRegex, '<pre>$1</pre>');
+    markdownText = markdownText.replace(preformattedRegex, format === 'ansi' ? '\x1b[7m$1\x1b[27m' : '<pre>$1</pre>');
 
     // Заміна символу нового рядка на тег <br> для збереження переносу рядків
     markdownText = markdownText.replace(/\n/g, '<br>');
@@ -33,7 +33,7 @@ function markdownToHTML(markdownText) {
 }
 
 // Функція для зчитування файлу та обробки його змісту
-function processFile(filePath, outputPath) {
+function processFile(filePath, outputPath, format) {
     // Зчитування вмісту файлу
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
@@ -41,8 +41,8 @@ function processFile(filePath, outputPath) {
             process.exit(1); // Завершення з помилкою
         }
 
-        // Перетворення Markdown в HTML
-        const htmlContent = markdownToHTML(data);
+        // Перетворення Markdown в HTML з вибором формату
+        const htmlContent = markdownToHTML(data, format);
 
         // Виведення HTML в консоль або запис у вихідний файл
         if (outputPath) {
@@ -63,6 +63,8 @@ function processFile(filePath, outputPath) {
 const args = process.argv.slice(2);
 const inputFile = args[0];
 const outputFile = args[1];
+const formatIndex = args.indexOf('--format');
+const format = formatIndex !== -1 && args[formatIndex + 1] ? args[formatIndex + 1].toLowerCase() : 'ansi';
 
 // Перевірка, чи був переданий шлях до вхідного файлу
 if (!inputFile) {
@@ -70,5 +72,5 @@ if (!inputFile) {
     process.exit(1); // Завершення з помилкою
 }
 
-// Обробка вхідного файлу
-processFile(inputFile, outputFile);
+// Обробка вхідного файлу з вибором формату виводу
+processFile(inputFile, outputFile, format);
